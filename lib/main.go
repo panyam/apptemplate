384b66edcd457758333188c5032dcdf1b27f46b8
Sri Panyam <sri.panyam@gmail.com>
2025-07-13 00:58:27 -0700

    Added lib entrypoint

---
+++ lib/main.go
@@ -0,0 +1,5 @@
+package lib
+
+// Typically this folder is a place to put "library" files exposed by your app.
+// If you are a pure service there may not be much.  But if you have a simulator or game etc then you would do a lot of
+// the thing here and expose it as a wasm to make life a lot easier
